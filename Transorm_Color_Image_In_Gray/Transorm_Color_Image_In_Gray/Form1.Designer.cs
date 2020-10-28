ca99107fddb1b42c95279e8aa8226bcc76282cb8
Killap888 <72222479+Killap888@users.noreply.github.com>
2020-10-28 10:05:11 +0300

    Add files via upload

---
+++ Transorm_Color_Image_In_Gray/Transorm_Color_Image_In_Gray/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Transorm_Color_Image_In_Gray
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxInput = new System.Windows.Forms.PictureBox();
+            this.pictureBoxOutput = new System.Windows.Forms.PictureBox();
+            this.download_image = new System.Windows.Forms.Button();
+            this.convert_image = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxInput)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxOutput)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxInput
+            // 
+            this.pictureBoxInput.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBoxInput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxInput.InitialImage = null;
+            this.pictureBoxInput.Location = new System.Drawing.Point(13, 13);
+            this.pictureBoxInput.Name = "pictureBoxInput";
+            this.pictureBoxInput.Size = new System.Drawing.Size(617, 695);
+            this.pictureBoxInput.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxInput.TabIndex = 0;
+            this.pictureBoxInput.TabStop = false;
+            // 
+            // pictureBoxOutput
+            // 
+            this.pictureBoxOutput.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBoxOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxOutput.Location = new System.Drawing.Point(663, 13);
+            this.pictureBoxOutput.Name = "pictureBoxOutput";
+            this.pictureBoxOutput.Size = new System.Drawing.Size(617, 695);
+            this.pictureBoxOutput.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxOutput.TabIndex = 1;
+            this.pictureBoxOutput.TabStop = false;
+            // 
+            // download_image
+            // 
+            this.download_image.Location = new System.Drawing.Point(127, 714);
+            this.download_image.Name = "download_image";
+            this.download_image.Size = new System.Drawing.Size(384, 49);
+            this.download_image.TabIndex = 2;
+            this.download_image.Text = "Загрузить изображение";
+            this.download_image.UseVisualStyleBackColor = true;
+            this.download_image.Click += new System.EventHandler(this.download_image_Click);
+            // 
+            // convert_image
+            // 
+            this.convert_image.Location = new System.Drawing.Point(781, 714);
+            this.convert_image.Name = "convert_image";
+            this.convert_image.Size = new System.Drawing.Size(384, 49);
+            this.convert_image.TabIndex = 3;
+            this.convert_image.Text = "Преобразовать изображение";
+            this.convert_image.UseVisualStyleBackColor = true;
+            this.convert_image.Click += new System.EventHandler(this.convert_image_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(1299, 776);
+            this.Controls.Add(this.convert_image);
+            this.Controls.Add(this.download_image);
+            this.Controls.Add(this.pictureBoxOutput);
+            this.Controls.Add(this.pictureBoxInput);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "Преобразование изображения из цветного в ЧБ";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxInput)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxOutput)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxInput;
+        private System.Windows.Forms.PictureBox pictureBoxOutput;
+        private System.Windows.Forms.Button download_image;
+        private System.Windows.Forms.Button convert_image;
+    }
+}
+
---
+++ Transorm_Color_Image_In_Gray/Transorm_Color_Image_In_Gray/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Transorm_Color_Image_In_Gray
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        OpenFileDialog ofd = new OpenFileDialog();
+        Bitmap image;
+
+        private void download_image_Click(object sender, EventArgs e)// кнопка загрузки изображения
+        {
+            ofd.Filter = "Image Files(*.BMP;*.JPG;*.PNG)|*.BMP;*.JPG;*.PNG";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    image = new Bitmap(ofd.FileName);
+                    pictureBoxInput.Image = image;                    
+
+                }
+                catch
+                {
+                    DialogResult rezult = MessageBox.Show("Невозможно открыть выбранный файл",
+                    "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+        }
+       
+        private void convert_image_Click(object sender, EventArgs e)
+        {
+            if (pictureBoxInput.Image != null) // если изображение в pictureBoxInput имеется
+            {               
+                ProcessUsingLockbits(image);
+            }
+
+            // выводим черно-белый Bitmap в pictureBoxOutput
+            pictureBoxOutput.Image = image;
+        
+        }
+
+
+        public void ProcessUsingLockbits(Bitmap processedBitmap)
+        {
+            BitmapData bitmapData = processedBitmap.LockBits(new Rectangle(0, 0, processedBitmap.Width, processedBitmap.Height), ImageLockMode.ReadWrite, processedBitmap.PixelFormat);
+
+            int bytesPerPixel = Bitmap.GetPixelFormatSize(processedBitmap.PixelFormat) / 8;
+            int byteCount = bitmapData.Stride * processedBitmap.Height;
+            byte[] pixels = new byte[byteCount];
+            IntPtr ptrFirstPixel = bitmapData.Scan0;
+            Marshal.Copy(ptrFirstPixel, pixels, 0, pixels.Length);
+            int heightInPixels = bitmapData.Height;
+            int widthInBytes = bitmapData.Width * bytesPerPixel;
+
+            //Color ArGBformat;
+
+            for (int y = 0; y < heightInPixels; y++)
+            {
+                int currentLine = y * bitmapData.Stride;
+                for (int x = 0; x < widthInBytes; x = x + bytesPerPixel)
+                {
+                    int oldBlue = pixels[currentLine + x];
+                    int oldGreen = pixels[currentLine + x + 1];
+                    int oldRed = pixels[currentLine + x + 2];                    
+
+                    int avrColor = (oldBlue + oldGreen + oldRed) / 3;
+
+                    int newBlue = avrColor;
+                    int newGreen = avrColor;
+                    int newRed = avrColor;
+
+                    //ArGBformat = Color.FromArgb(oldBlue, oldGreen, oldRed);
+
+                    // calculate new pixel value
+                    pixels[currentLine + x] = (byte)newBlue;
+                    pixels[currentLine + x + 1] = (byte)newGreen;
+                    pixels[currentLine + x + 2] = (byte)newRed;
+
+                    
+
+                }
+            }
+
+            
+            Marshal.Copy(pixels, 0, ptrFirstPixel, pixels.Length);
+            processedBitmap.UnlockBits(bitmapData);
+        }
+
+
+
+
+
+
+    }
+}
